3b8fd3be5d0b2f6f69305acff9e53b3c8fc876a6
dharshini <dharshini.22cs@licet.ac.in>
2021-06-15 20:55:51 +0530

    Create Day1-Program.py

---
+++ Day1-Program.py
@@ -0,0 +1,26 @@
+#Day1
+#Helloworld
+print("Helloworld")
+
+#max func
+value1 = 5
+value2 = 7
+maximum = max(value1, value2)
+print(maximum)
+
+#print string
+my_string = 'best'
+print(my_string)
+
+#factorial
+import math
+def factorial(n):
+    return (math.factorial(n))
+
+num = 4
+print("Factorial is", math.factorial(num))
+
+#addition
+c = value1+value2
+print(c)
+
