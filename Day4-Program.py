4062c6fa524d247e6b0a57adf95cfa2a26c00c43
dharshini <dharshini.22cs@licet.ac.in>
2021-06-15 20:04:23 +0530

    Create Day4-Program.py

---
+++ Day4-Program.py
@@ -0,0 +1,71 @@
+#variable
+number_of_wheels = 2
+print(number_of_wheels)
+
+#Assigning Values to Variables 
+#integer
+print(123123123123123123123123123123123123123123123123 + 2)
+
+#float
+pie = 3.14
+print(pie)
+
+#Complex numbers are specified as <real part>+<imaginary part>j
+complex = 5+10j
+print(complex)
+
+#Strings
+value = 'Atlas'
+print(value)
+
+#Boolean
+state= True
+print(state)
+
+#Multiple assignment
+
+c,d,e = 60,5.4,"hello"
+print(c)
+print(d)
+print(d)
+
+ #Excersie
+#1 Create three variables (a,b,c) to same value of any integer & 
+#do the following
+#a) Divide a by 10
+#b) Multiply b by 50
+#c) Add c value by 60
+
+a = b = c = 100
+d = a/10
+e = b*50
+f = c + 60
+print(d)
+print(e)
+print(f)
+
+#2 Create a String variable of 5 characters and replace the 3rd
+#character with G
+
+word = "Strip"
+wish = word.replace('r','G')
+print(wish)
+
+#3Create two values (a,b) of int,float data type & convert the 
+#vise versa, Hint : convert a from int to float datatype & b from 
+#float to int datatype
+
+a = 5
+b = 10.5
+a = float(a)
+b = int(b)
+print(a)
+print(b)
+
+
+
+
+
+
+
+
