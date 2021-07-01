1c5011db29863e60ceda680ecbbe74fabb06cc48
dharshini <dharshini.22cs@licet.ac.in>
2021-07-01 23:50:03 +0530

    Create Day19-Program.py

---
+++ Day19-Program.py
@@ -0,0 +1,26 @@
+from openpyxl import Workbook
+
+workbook = Workbook()
+spreadsheet = workbook.active
+
+spreadsheet["A1"] = "Name"
+spreadsheet["B1"] = "Python"
+spreadsheet["C1"] = "Chemistry"
+spreadsheet["D1"] = "Tamil"
+spreadsheet["A2"] = "vishika"
+spreadsheet["B2"] = "90"
+spreadsheet["C2"] = "100"
+spreadsheet["D2"] = "98"
+spreadsheet["A3"] = "Dharshka"
+spreadsheet["B3"] = "96"
+spreadsheet["C3"] = "90"
+spreadsheet["D3"] = "95"
+
+m_row = spreadsheet.max_row
+# Loop will print all values
+# of first column
+for i in range(1, m_row + 1):
+    cell_obj = spreadsheet.cell(row = i, column = 1)
+    print(cell_obj.value)
+
+workbook.save(filename="HelloWorld.xlsx")
