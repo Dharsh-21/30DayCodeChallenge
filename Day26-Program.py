8acf1eaffae619b5a8ac61b0dcfd07eba1ccaafb
dharshini <dharshini.22cs@licet.ac.in>
2021-07-11 13:27:48 +0530

    Create Day26-Program.py

---
+++ Day26-Program.py
@@ -0,0 +1,16 @@
+
+#Day26
+#1  Enumerate a python list and try to print the counter with the list value
+
+
+heir = ['bastard', 'jon snow', 'aegon targaryen']
+result = enumerate(heir)
+print(type(result))
+print(list(result))
+
+#2  Enumerate a python tuple and try to print the counter with the tuple value
+periodic = ['H','He','Li','Be','B','C','N','O','F','Ne']
+result = enumerate(periodic)
+print(type(result))
+print(tuple(result))
+
