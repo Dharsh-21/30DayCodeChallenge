9b7cf9661b602bf5d5a2051dc23a4c0dd6b9cc81
dharshini <dharshini.22cs@licet.ac.in>
2021-07-19 00:25:41 +0530

    Update Day28-Program.py

--- Day28-Program.py
+++ Day28-Program.py
@@ -10,21 +10,21 @@ logging.basicConfig(level=logging.DEBUG,
 class MyThread(threading.Thread):
 
     def __init__(self, group=None, target=None, name=None,
-                 args=(), kwargs=None, verbose=None):
-	super(MyThread,self).__init__(group=group, target=target, 
+                 args=(), kwargs=None, verbose=None): 
+        super(MyThread,self).__init__(group=group, target=target, 
 			              name=name, verbose=verbose)
 	self.args = args
 	self.kwargs = kwargs
 	return
 
     def run(self):
-	logging.debug('running with %s and %s', self.args, self.kwargs)
-	return
+	    logging.debug('running with %s and %s', self.args, self.kwargs)
+	    return
 
 if __name__ == '__main__':
     for i in range(3):
-	t = MyThread(args=(i,), kwargs={'a':1, 'b':2})
-	t.start()
+	    t = MyThread(args=(i,), kwargs={'a':1, 'b':2})
+	    t.start()
 
 #2 Instantiate the subclass and trigger the thread.
 
